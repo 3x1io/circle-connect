81d85606c6a8a68d8abd0c8c0c2ca41e7810e27d
Fady Mondy <engfadymondy@gmail.com>
2025-03-21 16:27:59 +0200

    last update

--- app/Filament/Widgets/ActionTableWidget.php
+++ app/Filament/Widgets/ActionTableWidget.php
@@ -4,7 +4,9 @@ namespace App\Filament\Widgets;
 
 use App\Models\Account;
 use App\Models\AccountMeta;
+use Filament\Actions\EditAction;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TimePicker;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Notifications\Notification;
@@ -60,6 +62,7 @@ class ActionTableWidget extends BaseWidget
                             ])
                             ->label('Response')
                             ->required(),
+                        Textarea::make('key_value')->label('Description'),
                         DatePicker::make('date')->default(now()->toDateString()),
                         TimePicker::make('time')->default(now()->toTimeString()),
                     ])
@@ -91,10 +94,72 @@ class ActionTableWidget extends BaseWidget
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ])
+            ->actions([
+                Tables\Actions\EditAction::make('edit')
+                    ->tooltip(__('filament-actions::edit.single.label'))
+                    ->iconButton()
+                    ->form([
+                        ToggleButtons::make('key')
+                            ->icons([
+                                'call' => 'heroicon-o-phone',
+                                'event' => 'heroicon-o-exclamation-circle',
+                            ])
+                            ->colors([
+                                'call' => 'success',
+                                'event' => 'warning',
+                            ])
+                            ->default('call')
+                            ->inline()
+                            ->options([
+                                'call' => 'Call',
+                                'event' => 'Event',
+                            ])
+                            ->label('Action')
+                            ->required(),
+                        ToggleButtons::make('response')
+                            ->colors([
+                                'ok' => 'success',
+                                'no-response' => 'danger',
+                            ])
+                            ->icons([
+                                'ok' => 'heroicon-o-check-circle',
+                                'no-response' => 'heroicon-o-x-circle',
+                            ])
+                            ->default('ok')
+                            ->inline()
+                            ->options([
+                                'ok' => 'Ok',
+                                'no-response' => 'No Response',
+                            ])
+                            ->label('Response')
+                            ->required(),
+                        Textarea::make('value')->label('Description'),
+                        DatePicker::make('date')->default(now()->toDateString()),
+                        TimePicker::make('time')->default(now()->toTimeString()),
+                    ])
+                    ->action(function (array $data, $record): void {
+                        $record->update([
+                            'key' => $data['key'],
+                            'value' => $data['value'],
+                            'response' => $data['response'],
+                            'date' => $data['date'],
+                            'time' => $data['time'],
+                        ]);
+
+                        Notification::make()
+                            ->title('Success')
+                            ->body('Data saved successfully!')
+                            ->success()
+                            ->send();
+                    })
+            ])
             ->columns([
-                Tables\Columns\TextColumn::make('date'),
-                Tables\Columns\TextColumn::make('time'),
+                Tables\Columns\TextColumn::make('date')->sortable(),
+                Tables\Columns\TextColumn::make('time')->sortable(),
+                Tables\Columns\TextColumn::make('key_value')->label('Description')->searchable()
+                    ->description(fn ($record) => $record->value),
                 Tables\Columns\TextColumn::make('key')
+                    ->label('Action')
                     ->formatStateUsing(fn ($state): string => str($state)->title())
                     ->color(fn ($record) => match ($record->key) {
                         'call' => 'success',
@@ -108,6 +173,7 @@ class ActionTableWidget extends BaseWidget
                     })
                     ->badge(),
                 Tables\Columns\TextColumn::make('response')
+                    ->sortable()
                     ->formatStateUsing(fn ($state): string => str($state)->title())
                     ->color(fn ($record) => match ($record->response) {
                         'ok' => 'success',
